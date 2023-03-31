48d675ab130238feb056cc13b2a9f52309e285e4
ln53491 <luka.nestic555@gmail.com>
2023-03-31 15:47:50 +0200

    Ivica Todoric test commit 3

--- fertrade/src/main/java/org/example/Main.java
+++ fertrade/src/main/java/org/example/Main.java
@@ -2,6 +2,6 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("FerTrade je najbolji!!!");
+        System.out.println("FerTrade je najbolji!");
     }
 }
